ed8b77458657abc7b47aa6c82934a0fc2404b217
johann <johann.tixier91@gmail.com>
2025-12-16 20:46:22 +0100

    responsive complet fini

--- App/menuderoulant.js
+++ App/menuderoulant.js
@@ -2,13 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const dropdown = document.querySelector(".dropdown");
     const btn = dropdown.querySelector(".dropdown-btn");
     const box = dropdown.querySelector(".dropdown-menu");
+    const hamBTN = document.querySelector(".menuhamburger");
+    const boxham = document.querySelector(".nav-links-mobile");
 
-    if (!btn || !box) {
+    if (!btn || !box || !hamBTN || !boxham) {
         console.error("❌ Impossible de trouver le bouton ou le menu !");
         return;
     }
 
     btn.addEventListener("click", () => {
         box.classList.toggle("show");
+    });
+
+    hamBTN.addEventListener("click", () => {
+        boxham.classList.toggle("show");
     });
 });
